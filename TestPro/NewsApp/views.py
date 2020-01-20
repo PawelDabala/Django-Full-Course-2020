496b00ca2ffcc8067872096d43ba1f917cd7eb40
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-01-20 16:55:05 +0100

    105:55 taplates inheritance

--- TestPro/NewsApp/views.py
+++ TestPro/NewsApp/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse("<h1>This is our home page<h1>")
+    return render(request, 'index.html')
 
 
 def News(request):
-    return HttpResponse("<h1> This is our latest news<h1>")
+    return render(request, 'news.html')
 
 
 def Contact(request):
-    return HttpResponse("<h1> This is contact us page <h1>")
+    return render(request, 'contact.html')
