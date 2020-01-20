5158ef7872c04ec6dce4afecf6d4fd5ff6e28402
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-01-20 15:34:49 +0100

    42:36 What are django Models

--- TestPro/NewsApp/urls.py
+++ TestPro/NewsApp/urls.py
@@ -1,22 +1,10 @@
-"""TestPro URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.urls import path
-from .views import News
+from .views import News, Contact, Home
 
 urlpatterns = [
-    path('news/', News, name='News'),
+    path('', Home, name='home'),
+    path('news/', News, name='news'),
+    path('contact/', Contact, name='contact')
 ]
--- TestPro/NewsApp/views.py
+++ TestPro/NewsApp/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
+def Home(request):
+    return HttpResponse("<h1>This is our home page<h1>")
+
 
 def News(request):
     return HttpResponse("<h1> This is our latest news<h1>")
-    
+
+
+def Contact(request):
+    return HttpResponse("<h1> This is contact us page <h1>")
