44ede6281d2a650f7e4c19dc76ead5c130035d42
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-01-21 10:49:29 +0100

    128:04 For loop in Django Template

--- TestPro/NewsApp/views.py
+++ TestPro/NewsApp/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def Home(request):
-    return render(request, 'index.html')
+
+    context = {
+        "name": "Pawel",
+        "number": 2332123
+    }
+
+    return render(request, 'index.html', context=context)
 
 
 def News(request):
-    return render(request, 'news.html')
+    context = {
+        "list": ["Python", "Java", "C++", "C#", "Ruby"]
+    }
+    return render(request, 'news.html', context=context)
 
 
 def Contact(request):
+
     return render(request, 'contact.html')
