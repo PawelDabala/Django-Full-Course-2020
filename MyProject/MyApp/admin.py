42d41d7bf73054c35084f3f7416bc9dfd423cf03
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-01-31 10:38:18 +0100

    324:44 Many to One RelationShip continue

--- MyProject/MyApp/admin.py
+++ MyProject/MyApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Publication, Article
+from .models import Publication, Article, Article2, Reporter
 
 # Register your models here.
 admin.site.register(Publication)
 admin.site.register(Article)
+admin.site.register(Article2)
+admin.site.register(Reporter)
--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -24,21 +24,42 @@ class Publication(models.Model):
 class Article(models.Model):
     """Model definition for Article."""
 
-
     # TODO: Define fields here
     headline = models.CharField(max_length=300)
     publications = models.ManyToManyField(Publication)
-    
-
 
     class Meta:
         """Meta definition for Article."""
         ordering = ('headline',)
-        
-
 
     def __str__(self):
-        
+
         return self.headline
 
 
+class Reporter(models.Model):
+    """Model definition for Reporter."""
+
+    # TODO: Define fields here
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Article2(models.Model):
+    """Model definition for Article2."""
+
+    # TODO: Define fields here
+    headline = models.CharField(max_length=30)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('headline',)
+
+    def __str__(self):
+        """Unicode representation of Article2."""
+        return self.headline
